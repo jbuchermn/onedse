4106ce385c7d9ab95e75dd58b8e159d8d3adee2e
Jonas Bucher <j.bucher.mn@gmail.com>
2017-12-18 17:54:59 +0100

    download script

---
+++ server/dependencies/download.sh
@@ -0,0 +1,10 @@
+wget http://www.alglib.net/translator/re/alglib-3.12.0.cpp.gpl.tgz
+tar -xvf alglib-3.12.0.cpp.gpl.tgz
+mv cpp/src alglib/alglib
+rm -rf cpp
+rm -f alglib-3.12.0.cpp.gpl.tgz
+
+wget http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz
+tar -xvf 3.3.4.tar.gz
+mv eigen-eigen-5a0156e40feb eigen
+rm -f 3.3.4.tar.gz
